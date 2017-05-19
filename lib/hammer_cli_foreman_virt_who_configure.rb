4ab43b77ee4dfd05198ccbacabda53fa16f2f7e4
Tomas Strachota <tstrachota@redhat.com>
2017-05-19 13:34:30 +0200

    i18n - initial setup

--- lib/hammer_cli_foreman_virt_who_configure.rb
+++ lib/hammer_cli_foreman_virt_who_configure.rb
@@ -1,10 +1,9 @@
 require 'hammer_cli'
 require 'hammer_cli_foreman'
+require 'hammer_cli_foreman_virt_who_configure/i18n'
 
 module HammerCLIForemanVirtWhoConfigure
-
   HammerCLI::MainCommand.lazy_subcommand('virt-who-config', _("Manage Virt Who configurations"),
     'HammerCLIForemanVirtWhoConfigure::VirtWhoConfig', 'hammer_cli_foreman_virt_who_configure/config'
   )
-
 end
---
+++ lib/hammer_cli_foreman_virt_who_configure/i18n.rb
@@ -0,0 +1,29 @@
+require 'hammer_cli/i18n'
+
+module HammerCLIForemanVirtWhoConfigure
+  module I18n
+    class LocaleDomain < HammerCLI::I18n::LocaleDomain
+      def translated_files
+        Dir.glob(File.join(File.dirname(__FILE__), '../**/*.rb'))
+      end
+
+      def locale_dir
+        File.join(File.dirname(__FILE__), '../../locale')
+      end
+
+      def domain_name
+        'hammer-cli-foreman-virt-who-configure'
+      end
+    end
+
+    class SystemLocaleDomain < LocaleDomain
+      def locale_dir
+        '/usr/share/locale'
+      end
+    end
+  end
+end
+
+domains = [HammerCLIForemanVirtWhoConfigure::I18n::LocaleDomain.new, HammerCLIForemanVirtWhoConfigure::I18n::SystemLocaleDomain.new]
+domain = domains.find { |d| d.available? }
+HammerCLI::I18n.add_domain(domain) if domain
